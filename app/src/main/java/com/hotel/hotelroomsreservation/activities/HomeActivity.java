94da46fbf0fa8eb2e6407e3d38faec91a879006f
Anna-Kohanova <anitacx88@gmail.com>
2016-09-30 14:41:18 +0300

    Add OptionsMenu.

--- app/src/main/java/com/hotel/hotelroomsreservation/activities/HomeActivity.java
+++ app/src/main/java/com/hotel/hotelroomsreservation/activities/HomeActivity.java
@@ -2,7 +2,12 @@ package com.hotel.hotelroomsreservation.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.hotel.hotelroomsreservation.R;
 
@@ -15,5 +20,29 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                // Activity Settings
+                return true;
+
+            case R.id.about_app:
+                // Activity AboutApp
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/hotel/hotelroomsreservation/activities/MainActivity.java
+++ app/src/main/java/com/hotel/hotelroomsreservation/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.hotel.hotelroomsreservation.R;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
         openHomeActivity();
     }
 
