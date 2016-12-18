47d08036a27cf77484726742a302d279b4690a90
Anna-Kohanova <anitacx88@gmail.com>
2016-12-18 12:30:07 +0300

    Change AppWelcomeActivity.

--- app/src/main/java/com/hotel/hotelroomreservation/App.java
+++ app/src/main/java/com/hotel/hotelroomreservation/App.java
@@ -1,11 +1,7 @@
 package com.hotel.hotelroomreservation;
 
 import android.app.Application;
-import android.content.Context;
-import android.util.Log;
 
-import com.firebase.client.Firebase;
-import com.hotel.hotelroomreservation.constants.Addresses;
 import com.hotel.hotelroomreservation.utils.validations.ContextHolder;
 
 public class App extends Application {
--- app/src/main/java/com/hotel/hotelroomreservation/activities/AppWelcomeActivity.java
+++ app/src/main/java/com/hotel/hotelroomreservation/activities/AppWelcomeActivity.java
@@ -1,36 +1,17 @@
 package com.hotel.hotelroomreservation.activities;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.hotel.hotelroomreservation.R;
-import com.wang.avi.AVLoadingIndicatorView;
+import android.support.v7.app.AppCompatActivity;
 
 public class AppWelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_welcome);
 
-        AVLoadingIndicatorView avi = (AVLoadingIndicatorView) findViewById(R.id.avi);
-        startAnim(avi);
-        //TODO subscribe/unsubscribe in onPause/onResume
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                Intent i = new Intent(AppWelcomeActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }, 3000);
-    }
-
-    private void startAnim(AVLoadingIndicatorView avi) {
-        avi.show();
+        Intent i = new Intent(AppWelcomeActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
